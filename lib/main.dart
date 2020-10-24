637f3b2971db279899a947842e0343ca4162c9f2
pawiie <pawan5653989@gmail.com>
2020-10-24 15:49:41 +0530

    added diamond

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,17 @@ import 'package:flutter/material.dart';
 // main function is starting point for all our flutter apps
 void main() {
   runApp(MaterialApp(
-    home: Center(
-      child: Text("Hello There!"),
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey[100],
+      appBar: AppBar(
+        title: Text("I Am Rich"),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+        child: Image(
+          image: AssetImage('images/red_diamond.png'),
+        ),
+      ),
     ),
   ));
 }
